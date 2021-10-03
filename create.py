6317b78f20d64a155150abf083fe61a9dfb0599b
minugavand <91831080+minugavand@users.noreply.github.com>
2021-10-03 18:16:50 +0530

    Update create.py

--- create.py
+++ create.py
@@ -1,9 +1,7 @@
 import sys
-import os
 from github import Github
 from dotenv import load_dotenv
 
-load_dotenv()
 
 path = os.getenv("FILEPATH")
 username = os.getenv("USERNAME")
@@ -13,7 +11,6 @@ def create():
     folderName = str(sys.argv[1])
     os.makedirs(path + str(folderName))
     user = Github(username, password).get_user()
-    repo = user.create_repo(folderName)
     print("Succesfully created repository {}".format(folderName))
 
 if __name__ == "__main__":
